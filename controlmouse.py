080bad20e675ea49721003124fdf50d3d4cf06b1
U-MSI-Kelix\Felix Li <Felix Li@MSI-Kelix.(none)>
2014-03-08 15:05:18 -0800

    pushing original code for micromouse. control for forward, left, right, backward implemented. trying to figure out encoders

---
+++ controlmouse.py
@@ -0,0 +1,81 @@
+#Brainstorm features for classifying features (worth to implement?)
+#Numbers for each sensor: is it increasing, decreasing, staying the same? (discusses up vs down)
+#The average of all three sensors (if all of them are the same height and all active, most likely a line)
+#Circle: did it go back to where it was
+
+# What's being given? All three numbers at the same time
+# What can I do? Plot numbers, calculate all the features
+
+#Serial connection
+import serial
+#COM_LIST = ['COM3', 'COM4', 'COM5']
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = 0
+which_com = 0
+
+for i in range(0, 16):
+    try:
+        com = 'COM' + str(i)
+        ser = serial.Serial(com)
+    except:
+        pass
+    else:
+        which_com = com
+        print "COM PORT:", which_com, " Found!"
+        break;
+
+if which_com == 0:
+    print "No COM ports found!"
+
+import win32api, win32con, win32gui
+from time import sleep
+# def click(x,y):
+#     win32api.SetCursorPos((x,y))
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+import webbrowser
+new = 2 # open in a new tab, if possible
+
+# open a public URL, in this case, the webbrowser docs
+url = "http://google.com"
+
+import speech
+
+def move(x,y):
+    win32api.SetCursorPos((x,y))
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    #double click
+    #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+def get_pos():
+    flags, hcursor, (x,y) = win32gui.GetCursorInfo()
+    return x, y
+
+movement = 12
+
+def main():
+    
+
+    while(True):
+        #print current_x, current_y
+
+        character = raw_input('Enter your input:')
+
+        try:
+            ser.write(character);    
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            pass
+
+
+ 
+
+if __name__ == "__main__":
+    main();
---
+++ micromouse/micromouse.ino
@@ -0,0 +1,228 @@
+
+/*
+Sample Code to run the Sparkfun TB6612FNG 1A Dual Motor Driver using Arduino UNO R3
+ 
+ This code conducts a few simple manoeuvres to illustrate the functions:
+ - motorDrive(motorNumber, motorDirection, motorSpeed)
+ - motorBrake(motorNumber)
+ - motorStop(motorNumber)
+ - motorsStandby
+ 
+ Connections:
+ - Pin 3 ---> PWMA
+ - Pin 8 ---> AIN2
+ - Pin 9 ---> AIN1
+ - Pin 10 ---> STBY
+ - Pin 11 ---> BIN1
+ - Pin 12 ---> BIN2
+ - Pin 5 ---> PWMB
+ 
+ - Motor 1: A01 and A02
+ - Motor 2: B01 and B02
+ 
+ Encoder Values: 
+ For Motor B, 
+ 52, 50
+ 
+ For Motor A,
+ 46, 48
+ 
+ 
+ */
+
+//Define the Pins
+
+//Motor 1
+int pinAIN1 = 9; //Direction
+int pinAIN2 = 8; //Direction
+int pinPWMA = 3; //Speed
+
+int pinEncoderA1 = 46;
+int pinEncoderA2 = 48;
+
+//Motor 2
+int pinBIN1 = 11; //Direction
+int pinBIN2 = 12; //Direction
+int pinPWMB = 5; //Speed
+
+int pinEncoderB1 = 50;
+int pinEncoderB2 = 52;
+
+//Standby
+int pinSTBY = 10;
+
+//Constants to help remember the parameters
+static boolean turnCW = 0;  //for motorDrive function
+static boolean turnCCW = 1; //for motorDrive function
+static boolean motor1 = 0;  //for motorDrive, motorStop, motorBrake functions
+static boolean motor2 = 1;  //for motorDrive, motorStop, motorBrake functions
+
+int lastSendCommand = 0;
+int encoderA1 = 0;
+int encoderA2 = 0;
+int encoderB1 = 0;
+int encoderB2 = 0;
+
+void setup()
+{
+  //Set the PIN Modes
+  pinMode(pinPWMA, OUTPUT);
+  pinMode(pinAIN1, OUTPUT);
+  pinMode(pinAIN2, OUTPUT);
+
+  pinMode(pinPWMB, OUTPUT);
+  pinMode(pinBIN1, OUTPUT);
+  pinMode(pinBIN2, OUTPUT);
+
+  pinMode(pinSTBY, OUTPUT);
+  
+  pinMode(pinEncoderA1, INPUT);
+  pinMode(pinEncoderA2, INPUT);
+  pinMode(pinEncoderB1, INPUT);
+  pinMode(pinEncoderB2, INPUT);
+  
+  digitalWrite(pinSTBY, HIGH);
+
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  
+    if (Serial.available()) {
+     char character_received = Serial.read();
+     if ( character_received == 'w') { //go straight
+       motorDrive(motor1, turnCW, 255);
+       motorDrive(motor2, turnCW, 255);
+     
+     } else if (character_received == 'a') { //go left
+       motorDrive(motor1, turnCCW, 255);
+       motorDrive(motor2, turnCW, 255);
+     
+     
+     } else if (character_received == 's') { //go right
+       motorDrive(motor1, turnCCW, 255);
+       motorDrive(motor2, turnCCW, 255);
+     
+     
+     
+     } else if (character_received == 'd') { // go back
+       motorDrive(motor1, turnCW, 255);
+       motorDrive(motor2, turnCCW, 255);
+     
+     
+     } else if (character_received == 'b') {
+       motorsStandby();  
+     
+     } else if (character_received == 'c') {
+      motorBrake(motor1);
+      motorBrake(motor2);  
+    }
+   }
+   
+   //motorDrive(motor1, turnCW, 150);
+   //motorDrive(motor2, turnCW, 150);
+    
+  // handleEncoder();
+}
+
+void handleEncoder() {
+  encoderA1 = digitalRead(pinEncoderA1);
+  encoderA2 = digitalRead(pinEncoderA2);
+  encoderB1 = digitalRead(pinEncoderB1);
+  encoderB2 = digitalRead(pinEncoderB2);
+  
+  Serial.println("EncoderA1:" + String(encoderA1));
+  Serial.println("EncoderA2:" + String(encoderA2));
+  Serial.println("EncoderB1:" + String(encoderB1));
+  Serial.println("EncoderB2:" + String(encoderB2));
+  Serial.println();
+  
+  
+}
+
+void motorDrive(boolean motorNumber, boolean motorDirection, int motorSpeed)
+{
+  /*
+  This Drives a specified motor, in a specific direction, at a specified speed:
+   - motorNumber: motor1 or motor2 ---> Motor 1 or Motor 2
+   - motorDirection: turnCW or turnCCW ---> clockwise or counter-clockwise
+   - motorSpeed: 0 to 255 ---> 0 = stop / 255 = fast
+   */
+
+  boolean pinIn1;  //Relates to AIN1 or BIN1 (depending on the motor number specified)
+
+
+  //Specify the Direction to turn the motor
+  //Clockwise: AIN1/BIN1 = HIGH and AIN2/BIN2 = LOW
+  //Counter-Clockwise: AIN1/BIN1 = LOW and AIN2/BIN2 = HIGH
+  if (motorDirection == turnCW)
+    pinIn1 = HIGH;
+  else
+    pinIn1 = LOW;
+
+  //Select the motor to turn, and set the direction and the speed
+  if(motorNumber == motor1)
+  {
+    //Serial.println("Motor1:");
+    digitalWrite(pinAIN1, pinIn1);
+    digitalWrite(pinAIN2, !pinIn1);  //This is the opposite of the AIN1
+    analogWrite(pinPWMA, motorSpeed);
+  }
+  else
+  {
+    //Serial.println("Motor2:");
+    digitalWrite(pinBIN1, pinIn1);
+    digitalWrite(pinBIN2, !pinIn1);  //This is the opposite of the BIN1
+    analogWrite(pinPWMB, motorSpeed);
+  }
+
+
+
+  //Finally , make sure STBY is disabled - pull it HIGH
+  digitalWrite(pinSTBY, HIGH);
+  lastSendCommand = millis();
+}
+
+void motorBrake(boolean motorNumber)
+{
+  /*
+This "Short Brake"s the specified motor, by setting speed to zero
+   */
+
+  if (motorNumber == motor1)
+    analogWrite(pinPWMA, 0);
+  else
+    analogWrite(pinPWMB, 0);
+
+}
+
+
+void motorStop(boolean motorNumber)
+{
+  /*
+  This stops the specified motor by setting both IN pins to LOW
+   */
+  if (motorNumber == motor1) {
+    digitalWrite(pinAIN1, LOW);
+    digitalWrite(pinAIN2, LOW);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, LOW);
+    digitalWrite(pinBIN2, LOW);
+  } 
+}
+
+
+void motorsStandby()
+{
+  /*
+  This puts the motors into Standby Mode
+   */
+  digitalWrite(pinSTBY, LOW);
+}
+
+
+
