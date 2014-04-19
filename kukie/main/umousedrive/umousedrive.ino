eac56370cda0bbb3e0093230c280bd9806809d9c
Ferix9288 <felix.li@berkeley.edu>
2014-04-19 12:58:33 -0700

    straight is not straight

--- kukie/main/umousedrive/umousedrive.ino
+++ kukie/main/umousedrive/umousedrive.ino
@@ -20,7 +20,24 @@ Encoder lenc(dwheelmm);
 
 Driver umouse(ain1, ain2, pwma, bin1, bin2, pwmb);
 
+#define KP_FRONT_NO_SIDES .6 
+#define KD_FRONT_NO_SIDES 0
+
+#define KP_FRONT_AND_LEFT .7
+#define KD_FRONT_AND_LEFT 0
+
+#define KP_FRONT_AND_RIGHT .7
+#define KD_FRONT_AND_RIGHT 0
+
+#define KP_FRONT_WITH_SIDES .5
+#define KD_FRONT_WITH_SIDES 0
+
 PID pid(kp, kd);
+PID pid_front_no_sides(KP_FRONT_NO_SIDES, KD_FRONT_NO_SIDES);
+PID pid_front_with_sides(KP_FRONT_WITH_SIDES, KD_FRONT_WITH_SIDES);
+PID pid_front_and_left(KP_FRONT_AND_LEFT, KD_FRONT_AND_LEFT);
+PID pid_front_and_right(KP_FRONT_AND_RIGHT, KD_FRONT_AND_RIGHT);
+
 
 float arr[30];
 int arrlen = -1;
@@ -37,6 +54,33 @@ volatile unsigned long stopEdge = 0;
 
 int pwmIncr = 10;
 
+#define FRONT_WITH_SIDES 0
+#define FRONT_NO_SIDES 1
+#define FRONT_AND_LEFT 2
+#define FRONT_AND_RIGHT 3
+#define BOTH_DIAGONALS 4
+#define LEFT_WALL 5
+#define RIGHT_WALL 6
+#define NO_WALLS 7
+int forwardState = NO_WALLS;
+int nextState = NO_WALLS;
+int prevState = NO_WALLS;
+
+#define SEE_FRONT 20
+#define LEFT_FRONT_IDEAL 12
+#define RIGHT_FRONT_IDEAL 12
+#define RIGHT_SIDE_IDEAL 2  //4,5 
+#define RIGHT_DIAGONAL_IDEAL 5 //9
+
+#define LEFT_SIDE_IDEAL 4 //4,5 
+#define LEFT_DIAGONAL_IDEAL 9
+
+
+#define FRONT_RIGHT_STOP 12
+#define RESILIENCE 0
+
+#define KP_STRAIGHT 1
+int resilience_counter = 0;
 void setup() {
   Serial.begin(9600);
   attachInterrupt(leftwheel, lincrement, RISING);
@@ -84,28 +128,38 @@ void disableTimerInterrupt() {
   
 }
 
+
+
+
 void loop()
 {
   //int cm = 90;
   //driveForward(cm * edgePerCm);
   //pidlib();
   //irdistlib();
+  //irvallib();
   //enclib();
   //drivelib();
-  //umouse.setPWM(10, 10);
+ // umouse.setPWM(30, 30);
+   //updateForwardState();
+   //Serial.println("State: " + String(forwardState));
+    //driveForward(edgePerSq, forwardSpeed);
+   driveStraight_Gyro();
+
   if (!done) {
     switchMode(SLOW);
-//    driveForward(edgePerSq);
-//    driveForward(edgePerSq, forwardSpeed);
-//    driveForward(edgePerSq, forwardSpeed);
-    turn(-90);
-    delay(1000);
-    turn(-90);
-    delay(1000);
-    turn(-90);
-    delay(1000);
+    //driveForward(edgePerSq, forwardSpeed);
+   // driveForward(edgePerSq, forwardSpeed);
+   // driveForward(edgePerSq, forwardSpeed);
 
-    turn(-90);
+//    turn(-90);
+//    delay(1000);
+//    turn(-90);
+//    delay(1000);
+//    turn(-90);
+//    delay(1000);
+//
+//    turn(-90);
 
 //    driveForward(edgePerSq, forwardSpeed);
 ////    driveForward(edgePerSq);
@@ -137,29 +191,97 @@ void loop()
 //  Serial.println(redge);
 }
 
+void driveStraight() {
+    int diff = redge - ledge;
+    int pidout = KP_STRAIGHT*(diff);
+    int pwmLeft = 20 + pidout; 
+    int pwmRight = 20 - pidout;
+    
+    if (pwmLeft <= 0) {
+      pwmLeft = 0;
+    } else if (pwmRight <= 0) {
+      pwmRight = 0;  
+    }
+    umouse.setPWM(pwmLeft, pwmRight);
+    Serial.println("ledge \t " + String(ledge) + "\t redge \t" + String(redge));
+}
+
+void driveStraight_Gyro() {
+  int minPWM = 20;
+  int maxPWM = 30;
+  
+  zRate = gyro.readZ() - zOff;
+  long difference = millis() - previousTime;      
+ 
+  actualDegreesChanged = degreesChanged/1000;
+  errorDegree = 0 - actualDegreesChanged; 
+  int dError = (errorDegree - previousDegreeError)/difference;
+
+ if (errorDegree > 0) {
+  pwmRight = errorDegree*KP_STRAIGHT+20;
+  if (pwmRight < minPWM) {
+    pwmRight = minPWM; 
+  } else if (pwmRight > maxPWM) {
+    pwmRight = maxPWM; 
+  }
+  
+  pwmLeft =  errorDegree*KP_STRAIGHT;
+  if (pwmLeft < minPWM) {
+    pwmLeft = minPWM; 
+  } else if (pwmLeft > maxPWM) {
+    pwmLeft = maxPWM; 
+  }
+ 
+ } else {
+  pwmLeft = (-1*errorDegree*KP_STRAIGHT)+20;
+  if (pwmLeft < minPWM) {
+    pwmLeft = minPWM; 
+  } else if (pwmLeft > maxPWM) {
+    pwmLeft = maxPWM; 
+  }
+  
+  pwmRight =  (-1*errorDegree*KP_STRAIGHT); 
+ if (pwmRight < minPWM) {
+    pwmRight = minPWM; 
+  } else if (pwmRight > maxPWM) {
+    pwmRight = maxPWM; 
+  }  
+ }
+ 
+ umouse.setPWM(pwmLeft, pwmRight);
+ //umouse.setPWM(20, 20);
+
+ previousTime = millis();
+ Serial.println(minPWM);
+ Serial.println(maxPWM);
+ Serial.println("Degrees Turned:" + String(actualDegreesChanged) + ";pwmLeft" + String(pwmLeft) + ";pwmRight" + String(pwmRight) + "; Rate: " + String(zRate) + "; Diff " + String(difference) );
+
+}
 void lincrement() {
   ledge++;
-  if (moving || turnRight) {
+  
+  if (forwardState == BOTH_DIAGONALS || forwardState == LEFT_WALL || forwardState == RIGHT_WALL || forwardState == NO_WALLS) { 
     if (ledge >= stopEdge) {
       umouse.brake();
       moving = false;
       turnRight = false;
       notComplete = false;
-
     }
-  } 
+  }
+  
+  
 }
 
 void rincrement() {
   redge++;
-  if (moving || turnLeft) {
-    if (redge >= stopEdge) {
-      umouse.brake();
-      moving = false;
-      turnLeft = false;
-      notComplete = false;
-    }
-  }
+//  if (moving || turnLeft) {
+//    if (redge >= stopEdge) {
+//      umouse.brake();
+//      moving = false;
+//      turnLeft = false;
+//      notComplete = false;
+//    }
+//  }
 }
 
 //Update gyro's angle rate. 
@@ -171,26 +293,207 @@ ISR(TIMER1_COMPA_vect)
   }
 }
 
+boolean leftWall() {
+  return (dlsensor.hasWall() && lsensor.hasWall());  
+}
+
+boolean rightWall() {
+  return (drsensor.hasWall() && rsensor.hasWall());
+}
+
+int updateForwardState() {
+  //Determine what state you're in first
+  double flcm = flsensor.getCm();
+  double frcm = frsensor.getCm();
+  if (flcm < SEE_FRONT && frcm < SEE_FRONT) {
+    
+    if (leftWall() && rightWall()) {
+      return FRONT_WITH_SIDES;
+    } else if (leftWall()) {
+      return FRONT_AND_LEFT;   
+    } else if ( rightWall() ) {
+      return FRONT_AND_RIGHT;   
+    } else {
+       return FRONT_NO_SIDES;      
+    }
+  } else if (leftWall() && rightWall()) {
+    return BOTH_DIAGONALS;
+  } else if (leftWall()) { //diagonal left or most left
+    return LEFT_WALL;
+  } else if (rightWall())  {
+    return RIGHT_WALL;  
+  } else {
+    return NO_WALLS;  
+  }
+    
+}
 // This function will drive the mouse forward for a specified number of edges.
 void driveForward(unsigned long deltaEdge, int fspeed) {
   stopEdge = ledge + deltaEdge;
   moving = true;
+  //forwardState = FRONT_NO_SIDES;
+  forwardState = updateForwardState();
+  resilience_counter = 0;
   while (moving) {
-    if (flsensor.hasWall() && frsensor.hasWall()) {
-      double lcm = flsensor.getCm();
-      double rcm = frsensor.getCm();
-      double diff = lcm - rcm;
-      int pidout = floor(diff*kpForward);
-      umouse.setPWM(fspeed - pidout, fspeed + pidout);
-    } else if (dlsensor.hasWall() && drsensor.hasWall()) { // do pid
-      double lcm = dlsensor.getCm();
-      double rcm = drsensor.getCm();
-      int pidout = floor(pid.getPIDterm(lcm, rcm));
-      umouse.setPWM(fspeed - pidout, fspeed + pidout);
-    } 
-      else {
-      umouse.setPWM(fspeed, fspeed);
-    //}
+      //irdistlib();
+
+    //output pwm
+    switch(forwardState) {
+      
+      case FRONT_NO_SIDES: {
+        double lcm = flsensor.getCm();
+        double rcm = frsensor.getCm();
+
+        //check to see if break out of this loop
+        if (lcm <= LEFT_FRONT_IDEAL || rcm <= RIGHT_FRONT_IDEAL) {
+          moving = false;
+          break;  
+        }
+        
+        umouse.setPWM(10, 10);
+        
+//             
+//        if (drsensor.hasWall()) {
+//          double drcm = drsensor.getCm();
+//          int pidout = floor(pid_front_no_sides.getPIDterm(rcm, drcm));
+//          umouse.setPWM(fspeed + pidout, fspeed - pidout);
+//        } else if (dlsensor.hasWall()) {
+//          double dlcm = dlsensor.getCm();
+//          int pidout = floor(pid_front_no_sides.getPIDterm(lcm, dlcm));
+//          umouse.setPWM(fspeed - pidout, fspeed + pidout);    
+//              
+//        } else {
+//        
+//          //if left sees longer distance, spin more with left wheel and vice versa
+//          int pidout = floor(pid_front_no_sides.getPIDterm(lcm, rcm));
+//          umouse.setPWM(fspeed + pidout, fspeed - pidout);
+//        }
+//        break; 
+      } 
+    
+
+     case FRONT_WITH_SIDES: {       
+        double front_lcm = flsensor.getCm();
+        double front_rcm = frsensor.getCm();
+        //check to see if break out of this loop
+        if (front_lcm <= LEFT_FRONT_IDEAL || front_rcm <= RIGHT_FRONT_IDEAL) {
+          moving = false;
+          break;  
+        }
+       
+       //stay in center
+       double lcm = dlsensor.getCm();
+       double rcm = drsensor.getCm();
+       int pidout = floor(pid_front_with_sides.getPIDterm(lcm, rcm));
+       umouse.setPWM(fspeed - pidout, fspeed + pidout);
+        
+       break;
+     } 
+     
+     case FRONT_AND_LEFT: {
+       
+        double front_rcm = frsensor.getCm();
+        //check to see if break out of this loop
+        if (front_rcm <= RIGHT_FRONT_IDEAL) {
+          moving = false;
+          break;  
+        }
+        //if leftmost sees shorter, then oriented too much right and vice versa if diagonal left
+       double lcm = lsensor.getCm();
+       double rcm = dlsensor.getCm();
+         
+       double diff_lcm = lcm - LEFT_SIDE_IDEAL;
+       double diff_rcm = rcm - LEFT_DIAGONAL_IDEAL;
+   
+       int pidout = floor(pid_front_and_left.getPIDterm(diff_lcm, diff_rcm));
+       umouse.setPWM(fspeed + pidout, fspeed - pidout);
+       break;  
+     }
+     
+     case FRONT_AND_RIGHT: {
+        double front_lcm = flsensor.getCm();
+        double front_rcm = frsensor.getCm();
+
+        //check to see if break out of this loop
+        if (front_lcm <= FRONT_RIGHT_STOP || front_rcm <= RIGHT_FRONT_IDEAL) {
+          moving = false;
+          break;  
+        }
+       
+       //if diagonal right sees shorter, then oriented too much left and vice versa if rightmost
+       double lcm = drsensor.getCm();
+       double rcm = rsensor.getCm();
+       
+       double diff_lcm = lcm - RIGHT_DIAGONAL_IDEAL;
+       double diff_rcm = rcm - RIGHT_SIDE_IDEAL;
+       
+       int pidout = floor(pid_front_and_right.getPIDterm(diff_lcm, diff_rcm));
+       umouse.setPWM(fspeed + pidout, fspeed - pidout);
+       break;  
+     }
+      
+      case BOTH_DIAGONALS: {
+        
+        //stay in center
+        double lcm = dlsensor.getCm();
+        double rcm = drsensor.getCm();
+        int pidout = floor(pid.getPIDterm(lcm, rcm));
+        umouse.setPWM(fspeed - pidout, fspeed + pidout);
+        break;  
+      }
+      
+      case LEFT_WALL: {
+        //if leftmost sees shorter, then oriented too much right and vice versa if diagonal left
+       double lcm = lsensor.getCm();
+       double rcm = dlsensor.getCm();
+         
+       double diff_lcm = lcm - LEFT_SIDE_IDEAL;
+       double diff_rcm = rcm - LEFT_DIAGONAL_IDEAL;
+   
+       int pidout = floor(pid.getPIDterm(diff_lcm, diff_rcm));
+       umouse.setPWM(fspeed + pidout, fspeed - pidout);
+       break;  
+    
+        break;  
+      }
+      
+      case RIGHT_WALL: {
+       //if diagonal right sees shorter, then oriented too much left and vice versa if rightmost
+       double lcm = drsensor.getCm();
+       double rcm = rsensor.getCm();
+       
+       double diff_lcm = lcm - RIGHT_DIAGONAL_IDEAL;
+       double diff_rcm = rcm - RIGHT_SIDE_IDEAL;
+       
+       int pidout = floor(pid.getPIDterm(diff_lcm, diff_rcm));
+       umouse.setPWM(fspeed + pidout, fspeed - pidout);
+       break;  
+    
+        break;  
+      }
+      
+      case NO_WALLS: { //use pid on encoder counts when time allows
+        umouse.setPWM(fspeed, fspeed);
+        break;       
+      }
+    }
+    
+    //updating state
+    nextState = updateForwardState();
+    if (nextState == prevState) {
+      resilience_counter += 1;  
+    } else {
+      resilience_counter = 0;
+    }
+    
+    if (resilience_counter >= RESILIENCE || nextState == NO_WALLS) {
+      forwardState = nextState;  
+    }
+//    } else if ( (forwardState == LEFT_WALL && !dlsensor.hasWall())  ||
+//              && drsensor.hasWall()
+
+    prevState = nextState;
+    Serial.println("State: " + String(forwardState));
   }
   umouse.brake();
   Serial.print(deltaEdge);
